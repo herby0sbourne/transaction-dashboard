30cf10d5027ddec63c50995fc9671ac7a1ace8d6
Herby <herbyosbourne31@gmail.com>
2022-12-10 15:36:23 -0500

    add import for redux store

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import globalReducer from 'state';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from './state/api';
 
 import App from './App';
 import './index.css';
@@ -10,8 +12,12 @@ import './index.css';
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
