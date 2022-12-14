5d4d33bb1eafeb5de4c39c90a817c67980d4e794
Herby <herbyosbourne31@gmail.com>
2022-12-14 15:56:43 -0500

    create overview and daily route

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,11 @@ import { themeSettings } from 'theme';
 import Layout from 'components/layout/index';
 import Dashboard from './pages/dashboard/index';
 import Products from './pages/products/Products';
+import Overview from './pages/overview/Overview';
 import Customers from './pages/customers/Customers';
 import Geography from './pages/geography/Geography';
 import Transactions from './pages/transactions/Transactions';
+import Daily from './pages/daily/Daily';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -29,6 +31,8 @@ function App() {
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
+              <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
             </Route>
           </Routes>
         </ThemeProvider>
