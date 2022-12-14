09b087fa3c73b3e1af049d34a8f0c963123c6c99
Herby <herbyosbourne31@gmail.com>
2022-12-14 10:10:30 -0500

    create sales route and getSales function

---
+++ server/controllers/salesController.js
@@ -0,0 +1,11 @@
+import OverallStats from '../models/overallStatsModel.js';
+
+export const getSales = async (req, res) => {
+  try {
+    const overallStats = OverallStats.find();
+
+    res.status(200).send(overallStats[0]);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
