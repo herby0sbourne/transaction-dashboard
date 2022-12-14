67cb56816f36f66773825ddaa0a2a698bcc33431
Herby <herbyosbourne31@gmail.com>
2022-12-14 10:09:44 -0500

    create overallStat Schema

---
+++ server/models/overallStatsModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const overallStatsSchema = new mongoose.Schema(
+  {
+    totalCustomers: Number,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: [
+      {
+        date: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    salesByCategory: {
+      type: Map,
+      of: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const OverallStats = mongoose.model('OverallStats', overallStatsSchema);
+
+export default OverallStats;
