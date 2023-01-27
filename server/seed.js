1648be9881b827278ed7ebf96179ad4e5014db03
Herby <herbyosbourne31@gmail.com>
2023-01-26 21:13:25 -0500

    fix missing seed data

--- server/seed.js
+++ server/seed.js
@@ -3,13 +3,17 @@ import {
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataAffiliateStat,
   dataUser,
 } from './data/index.js';
+
+import AffiliateStat from './models/AffiliateStat.js';
 import OverallStats from './models/overallStatsModel.js';
 import Product from './models/productModel.js';
 import ProductStat from './models/productStatModel.js';
 import Transaction from './models/transactionModel.js';
 import User from './models/userModel.js';
+AffiliateStat;
 
 const seedData = async (upload = false) => {
   try {
@@ -22,11 +26,13 @@ const seedData = async (upload = false) => {
     await Product.deleteMany();
     await Transaction.deleteMany();
     await OverallStats.deleteMany();
+    await AffiliateStat.deleteMany();
 
     console.log('DATA DELETED');
     console.log('UPLOADING DATA');
 
     await User.insertMany(dataUser);
+    await AffiliateStat.insertMany(dataAffiliateStat);
     await ProductStat.insertMany(dataProductStat);
     await Product.insertMany(dataProduct);
     await Transaction.insertMany(dataTransaction);
